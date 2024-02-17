7f348194193371d4b1166c7016980b3a64f5c093
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-17 11:04:49 +0000

    amended form slightly, removed IDs as these were already being generated with the controlId attribute

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,28 +2,34 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Contact() {
+
+  function submitForm(e) {
+    e.preventDefault();
+    console.log(e)
+
+  }
   return (
     <>
       <div className="container">
         <h1>Contact</h1>
         <Form>
-          <Form.Group className="mb-3" controlId="nameInput">
+          <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="email" placeholder="Name" />
+            <Form.Control type="text" placeholder="Name" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="emailInput">
+          <Form.Group className="mb-3" controlId="email">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="name@example.com" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="phoneInput">
+          <Form.Group className="mb-3" controlId="phone">
             <Form.Label>Contact Number</Form.Label>
-            <Form.Control type="email" placeholder="+44 01234 567 890" />
+            <Form.Control type="tel" placeholder="+44 01234 567 890" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
+          <Form.Group className="mb-3" controlId="message">
+            <Form.Label>Your Message</Form.Label>
             <Form.Control as="textarea" rows={3} />
           </Form.Group>
-          <Button variant="primary" type="submit" id="contactButton">
+          <Button variant="primary" type="submit" id="contactButton" onClick={submitForm}>
             Submit
           </Button>
         </Form>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,3 @@
-import Header from "./Header";
-
 function Home() {
   return (
     <>
