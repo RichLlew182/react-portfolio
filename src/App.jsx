4cb13d7ee531a3919de3f6c175097b6f8cb4a66b
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-17 10:21:24 +0000

    installed react-router-dom and imported hashrouter from it. Setup a router component in the app page. Added dummy content for all components listed in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import NavigationBar from './components/NavBar'
+import NavTabs from './components/NavBar'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
 
 function App() {
 
   return (
     <>
-      <NavigationBar />
+      <Router basename="/">
+        <NavTabs />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="projects/*" element={<Projects />} />
+        </Routes>
+
+      </Router>
     </>
   )
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,7 @@
-function Contact() {
+function Gallery() {
   return (
     <>
-      <h1>Contact</h1>
+      <h1>Gallery</h1>
       <p>
         No, no, no! Yes, yes. A bit. But she's got a wart. The swallow may fly south with the sun, and the house martin or the plover may seek warmer climes in winter, yet these are not strangers to our land. Knights of Ni, we are but simple travelers who seek the enchanter who lives beyond these woods.
       </p>
@@ -9,4 +9,4 @@ function Contact() {
   );
 }
 
-export default Contact;
+export default Gallery;
