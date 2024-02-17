9482881f9544af004e0a4a947ab9a344e5f3cf14
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-17 12:42:15 +0000

    added some example images to the images folder and linked to them in the json file

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -3,17 +3,19 @@ import Card from 'react-bootstrap/Card';
 
 function GalleryCard({ title, image, description, deployedURL, repoLink }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description}
-        </Card.Text>
-        <Button href={deployedURL} target="_blank" variant="primary">View Project</Button>
-        <Button href={repoLink} target="_blank" variant="primary">Repo Link</Button>
-      </Card.Body>
-    </Card>
+    <div className='col-md-6 col-lg-4'>
+      <Card>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {description}
+          </Card.Text>
+          <Button href={deployedURL} target="_blank" variant="primary">View Project</Button>
+          <Button href={repoLink} target="_blank" variant="primary">Repo Link</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
