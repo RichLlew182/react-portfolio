ab89e87ef931c522ec660e186b3e9ae7e8013f49
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-17 11:51:38 +0000

    added the map function to the projects file that iterates over the json file and passes in the props to the galleryCard component. This is now outputting the two cards onto the projects page

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,21 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import projectList from '../project-list.json'
 
-function GalleryCard() {
+function GalleryCard(props) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={projectList[0].image} />
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
-        <Card.Title>{projectList[0].title}</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          {projectList[0].description}
+          {props.description}
         </Card.Text>
-        <Button href={projectList[0].deployedURL} target="_blank" variant="primary">View Project</Button>
-        <Button href={projectList[0].repoLink} target="_blank" variant="primary">Repo Link</Button>
+        <Button href={props.deployedURL} target="_blank" variant="primary">View Project</Button>
+        <Button href={props.repoLink} target="_blank" variant="primary">Repo Link</Button>
       </Card.Body>
     </Card>
   );
 }
 
 export default GalleryCard;
+
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,17 @@
 import GalleryCard from "./GalleryCard";
+import projectList from '../project-list.json'
 
 function Projects() {
   return (
     <>
       <div className="container">
         <h1>Projects</h1>
-        <div>
-          <GalleryCard />
+        <div className="row">
+          {
+            projectList.map((project) => (
+              <GalleryCard key={project.id} title={project.title} description={project.description} image={project.image} deployedURL={project.deployedURL} repoLink={project.repoLink} />
+            ))
+          }
         </div>
       </div>
     </>
