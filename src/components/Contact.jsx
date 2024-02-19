f4cc6b6e18d81fca4d46403762a4b9ae4d7f224c
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-19 21:11:49 +0000

    updated contact form so that the top 3 fields are in a row

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function Contact() {
 
@@ -8,7 +10,6 @@ function Contact() {
   const thankYouMessage = document.querySelector('#thankYouMessage');
   const displayNone = { display: 'none' }
   const displayBlock = { display: 'block' }
-
 
   // Setting initial state to an object
   const [formData, setFormData] = useState({
@@ -33,42 +34,38 @@ function Contact() {
 
     event.preventDefault();
 
-    if (!formData.name || !formData.email || !formData.phone || !formData.message) {
-      alert('Looks like you missed a field')
-    }
 
-    else {
+    thankYouMessage.style = { displayBlock }
+    contactForm.style.display = 'none';
+  }
 
-      thankYouMessage.style = { displayBlock }
-      contactForm.style.display = 'none';
-    }
+  // setFormData({
+  //   name: '',
+  //   email: '',
+  //   phone: '',
+  //   message: ''
+  // });
 
-    // setFormData({
-    //   name: '',
-    //   email: '',
-    //   phone: '',
-    //   message: ''
-    // });
-
-  };
 
   return (
     <>
       <section className="container py-5 px-0">
         <h2 className="mb-4 display-5 fw-bold">Contact</h2>
         <Form id="contact-form">
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" name="name" placeholder="Name" value={formData.name} onChange={handleInputChange} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleInputChange} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="phone">
-            <Form.Label>Contact Number</Form.Label>
-            <Form.Control type="tel" name="phone" placeholder="+44 01234 567 890" onChange={handleInputChange} />
-          </Form.Group>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="name" placeholder="Name" value={formData.name} onChange={handleInputChange} required />
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="email">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleInputChange} />
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="phone">
+              <Form.Label>Contact Number</Form.Label>
+              <Form.Control type="tel" name="phone" placeholder="+44 01234 567 890" onChange={handleInputChange} />
+            </Form.Group>
+          </Row>
           <Form.Group className="mb-3" controlId="message">
             <Form.Label>Your Message</Form.Label>
             <Form.Control as="textarea" name="message" rows={3} onChange={handleInputChange} />
