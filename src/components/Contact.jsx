e5adc2d3f3c9f2d499b47ffb7a1b10dc066af5e1
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-19 20:13:57 +0000

    started adding some input change handlers and a form submit handler on the form to capture the user data

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,68 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 function Contact() {
 
-  function submitForm(e) {
-    e.preventDefault();
-    console.log(e)
+  // Setting initial state to an object
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  });
 
-  }
+  const handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+
+    // Alert the user their first and last name, clear `formData.firstName` and `formData.lastName`, clearing the inputs
+    alert(`Hello ${formData.name} ${formData.email} ${formData.phone} ${formData.message}`);
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    });
+
+
+
+  };
+
+
   return (
     <>
       <section className="container py-5 px-0">
         <h2 className="mb-4 display-5 fw-bold">Contact</h2>
-        <Form>
+        <Form id="contact-form">
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" placeholder="Name" />
+            <Form.Control type="text" name="name" placeholder="Name" value={formData.name} onChange={handleInputChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="email">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleInputChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="phone">
             <Form.Label>Contact Number</Form.Label>
-            <Form.Control type="tel" placeholder="+44 01234 567 890" />
+            <Form.Control type="tel" name="phone" placeholder="+44 01234 567 890" onChange={handleInputChange} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="message">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" name="message" rows={3} onChange={handleInputChange} />
           </Form.Group>
-          <Button variant="primary" type="submit" id="contactButton" onClick={submitForm}>
+          <Button variant="primary" type="submit" id="contactButton" onClick={handleFormSubmit}>
             Submit
           </Button>
         </Form>
