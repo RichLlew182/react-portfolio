ee8f84732b464ec8828c310b85f3d6bb2b846e01
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-20 21:48:01 +0000

    updated footer links to open in new tab and also changed colour of the links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,11 +11,11 @@ function Footer() {
         </div>
 
         <ul className="nav col-sm-6 justify-content-end list-unstyled d-flex">
-          <li className="ms-3"><a className="text-muted text-decoration-none" href="https://github.com/RichLlew182"><i
+          <li className="ms-3"><a className="text-decoration-none" target="_blank" href="https://github.com/RichLlew182"><i
             className="fa-brands fa-github me-2"></i>Github</a></li>
-          <li className="ms-3"><a className="text-muted text-decoration-none" href="https://github.com/RichLlew182"><i
+          <li className="ms-3"><a className="text-decoration-none" target="_blank" href="https://github.com/RichLlew182"><i
             className="fa-brands fa-linkedin  me-2"></i>Linkedin</a></li>
-          <li className="ms-3"><a className="text-muted text-decoration-none" href="./assets/files/richard-llewellyn-cv.pdf"><i className="fa-solid fa-file-lines me-2"></i>Resume</a></li>
+          <li className="ms-3"><a className="text-decoration-none" target="_blank" href="./assets/files/richard-llewellyn-cv.pdf"><i className="fa-solid fa-file-lines me-2"></i>Resume</a></li>
 
         </ul>
       </footer >
