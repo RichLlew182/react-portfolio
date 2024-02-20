4df0caa1c344217c462d847b5b11f1150214ea9c
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-02-20 09:25:33 +0000

    added validation to the form fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,6 +11,8 @@ function Contact() {
   const displayNone = { display: 'none' }
   const displayBlock = { display: 'block' }
 
+  const [validated, setValidated] = useState(false);
+
   // Setting initial state to an object
   const [formData, setFormData] = useState({
     name: '',
@@ -34,9 +36,17 @@ function Contact() {
 
     event.preventDefault();
 
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    } else {
+      thankYouMessage.style = { displayBlock }
+      contactForm.style.display = 'none';
+    }
 
-    thankYouMessage.style = { displayBlock }
-    contactForm.style.display = 'none';
+    setValidated(true);
+
   }
 
   // setFormData({
@@ -51,26 +61,38 @@ function Contact() {
     <>
       <section className="container py-5 px-0">
         <h2 className="mb-4 display-5 fw-bold">Contact</h2>
-        <Form id="contact-form">
+        <Form id="contact-form" noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Row className="mb-3">
             <Form.Group as={Col} md="4" controlId="name">
               <Form.Label>Name</Form.Label>
               <Form.Control type="text" name="name" placeholder="Name" value={formData.name} onChange={handleInputChange} required />
+              <Form.Control.Feedback type="invalid">
+                Please provide your name.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group as={Col} md="4" controlId="email">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleInputChange} />
+              <Form.Control type="email" name="email" placeholder="name@example.com" onChange={handleInputChange} required />
+              <Form.Control.Feedback type="invalid">
+                Please provide your email address.
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group as={Col} md="4" controlId="phone">
               <Form.Label>Contact Number</Form.Label>
-              <Form.Control type="tel" name="phone" placeholder="+44 01234 567 890" onChange={handleInputChange} />
+              <Form.Control type="tel" name="phone" placeholder="+44 01234 567 890" onChange={handleInputChange} required />
+              <Form.Control.Feedback type="invalid">
+                Please provide contact number.
+              </Form.Control.Feedback>
             </Form.Group>
           </Row>
           <Form.Group className="mb-3" controlId="message">
             <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" name="message" rows={3} onChange={handleInputChange} />
+            <Form.Control as="textarea" name="message" rows={3} onChange={handleInputChange} required />
+            <Form.Control.Feedback type="invalid">
+              Please let me know what you want to discuss.
+            </Form.Control.Feedback>
           </Form.Group>
-          <Button variant="primary" type="submit" id="contactButton" onClick={handleFormSubmit}>
+          <Button variant="primary" type="submit" id="contactButton">
             Submit
           </Button>
         </Form>
