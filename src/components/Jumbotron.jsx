2e2707819bd0325b9231ca7caaef7d2ddfe78b4f
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-04-10 11:25:41 +0100

    added surname to jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,7 +5,7 @@ function Jumbotron() {
         <div className="container py-0 py-sm-5 px-4 px-sm-0">
           <div className="row gx-5 d-flex flex-wrap align-items-center flex-column-reverse flex-md-row">
             <div className="col-md-6 col-lg-8 py-4  py-lg-0 d-flex justify-content-start">
-              <h1 className="display-4"><span className="d-block">Hi, I&apos;m Richard. </span><span className="d-block">A Front-End Developer and Designer.</span></h1>
+              <h1 className="display-4"><span className="d-block">Hi, I&apos;m Richard Llewellyn. </span><span className="d-block">A Front-End Developer and Designer.</span></h1>
 
             </div>
 
