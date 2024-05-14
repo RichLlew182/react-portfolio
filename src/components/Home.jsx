caf59180ceb3855a2d9e4c96b510671508e74e73
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-14 17:33:07 +0100

    added animations to the list elements that get created, just need to update json file now

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import Row from "react-bootstrap/Row";
 import Jumbotron from './Jumbotron'
 import { motion } from "framer-motion";
 import { Link, useNavigate } from 'react-router-dom';
+import skillsList from '../../data/skills.json'
 
 function Home() {
 
@@ -19,15 +20,29 @@ function Home() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.25 + 0.1 * index,
+      }
+    })
+  }
+
 
 
   return (
     <>
       <Jumbotron />
-      <motion.section initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} className="container py-5 px-4 px-sm-0 mt-lg-4">
+      <section className="container py-5 px-4 px-sm-0 mt-lg-4">
 
         <Row>
-          <div className="col-lg-12 col-xl-6 mb-5 mb-xl-0">
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1 }} className="col-lg-12 col-xl-6 mb-5 mb-xl-0">
             <div className="mb-4 pe-3">
               <h2 className="mb-4 display-5">About me</h2>
               <p>My name is Richard, and I am a skilled Front-End Developer and designer based in London. With a rich background spanning several years in the design industry, I am now eagerly seeking fresh challenges and exciting opportunities to leverage my expertise as a Front-End Developer.</p>
@@ -38,11 +53,23 @@ function Home() {
             <div className="btn-toolbar">
               <Link className="btn btn-primary me-3 border-0" onClick={handleProjectsClick} type="button">View Projects</Link><Link className="btn btn-secondary border-0 me-3" onClick={handleContactClick} type="button">Get in Touch</Link>
             </div>
-          </div>
+          </motion.div>
 
           <div className="col-lg-12 col-xl-6">
             <div className="h-100 ">
-              <h2 className="mb-4 display-5">My skills so far...</h2>
+              <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.25 }} className="mb-4 display-5">My skills so far...</motion.h2>
+
+              <ul className="list-group list-group-horizontal d-flex flex-wrap gap-3 pt-1">{
+                skillsList.map((skill, index) => (
+                  <motion.li className='list-group-item d-flex py-2 align-items-center rounded border-0' key={skill.id} variants={fadeInAnimationVariants} initial={"initial"} animate={"animate"} custom={index}>
+                    <img className="skill-icon" src={skill.icon} alt={skill.alt}></img>
+                    {skill.title}
+                  </motion.li>
+                ))
+              }
+              </ul>
+
+
               <ul className="list-group list-group-horizontal d-flex flex-wrap gap-3 pt-1">
                 <li className="list-group-item d-flex py-2 align-items-center rounded border-0"><img alt="HTML 5 Icon" className="skill-icon" src="./assets/icons/html-icon.png"></img>HTML5</li>
                 <li className="list-group-item d-flex py-2 align-items-center rounded border-0"><img alt="CSS 3 Icon" className="skill-icon" src="./assets/icons/css-icon.png"></img>CSS3</li>
@@ -63,7 +90,7 @@ function Home() {
           </div>
         </Row>
 
-      </motion.section >
+      </section >
 
 
     </>
