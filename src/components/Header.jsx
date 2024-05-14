fbf93c90e37d7d383891b135ab63499a60f0d47b
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-14 21:25:41 +0100

    adjusted layout of contact page and added more animations to elements on page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,18 +10,17 @@ function Header() {
       <header>
         <Navbar collapseOnSelect expand="lg" className="py-3 bg-white">
           <Container className="px-4 px-sm-0">
-            <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 2.5 }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
+            <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
               <Nav>
-                <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 2 }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 2.1 }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 2.2 }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        {/* <Jumbotron /> */}
       </header>
     </>
 
--- src/components/ProjectGallery.jsx
+++ src/components/ProjectGallery.jsx
@@ -11,7 +11,7 @@ const fadeInAnimationVariants = {
     opacity: 1,
     y: 0,
     transition: {
-      delay: 0.1 * index,
+      delay: 0.25 + 0.1 * index,
     }
   })
 }
@@ -22,11 +22,9 @@ function ProjectGallery() {
 
       <section id="projects" className="primary-green">
         <div className="container py-5 px-4 px-sm-0">
-          <h2 className="mb-4 display-5">Projects</h2>
+          <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.25 }} className="mb-4 display-5">Projects</motion.h2>
           <div className="row">
             {
-
-              // map function that iterates over the objects in the project-list JSON file and passes in the props to the Project component
               projectList.map((project, index) => (
                 <motion.div className='col-md-6 col-lg-4 my-3' key={project.id} variants={fadeInAnimationVariants} viewport={{ once: true }} whileInView={"animate"} initial={"initial"} custom={index}>
                   <ProjectCard key={project.id} title={project.title} description={project.description} image={project.image} altText={project.imageAltText} techOne={project.tech[0]} techTwo={project.tech[1]} techThree={project.tech[2]} deployedURL={project.deployedURL} repoLink={project.repoLink} />
