8a4bb6a479812e58230c4820d4f98a6228754779
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-14 14:44:05 +0100

    added new project images for devopps and poh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
-import ProjectGallery from './components/Project-Gallery'
+import ProjectGallery from './components/ProjectGallery'
 import Contact from './components/Contact'
 
 
