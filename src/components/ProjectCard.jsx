9c4b84203393ccf5ae1ae8b51b1a3b4fffb613ce
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-14 16:31:41 +0100

    added new code quiz image and re-ordered. Also added tech badges that pull info from the json file

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,7 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Badge, Card, Button } from 'react-bootstrap';
 
 // eslint-disable-next-line react/prop-types
-function ProjectCard({ title, image, description, deployedURL, altText }) {
+function ProjectCard({ title, image, description, deployedURL, altText, techOne, techTwo, techThree }) {
   return (
     <div>
       <Card className="h-100">
@@ -10,11 +9,12 @@ function ProjectCard({ title, image, description, deployedURL, altText }) {
           <Card.Img variant="top" src={image} alt={altText} />
         </a>
         <Card.Body className="p-4">
-          <Card.Title>{title}</Card.Title>
+          <Card.Title className="mb-3">{title}</Card.Title>
           <Card.Text>
             {description}
           </Card.Text>
           <Button href={deployedURL} target="_blank" className="me-3 border-0" variant="primary">View Project</Button>
+          <div className="technology mt-4">  <Badge bg="info">{techOne}</Badge>  <Badge bg="info">{techTwo}</Badge>  <Badge bg="info">{techThree}</Badge></div>
         </Card.Body>
       </Card>
     </div>
