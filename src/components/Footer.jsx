af86fd383ac8f1fe8f4534e5c5cb8734cb7107ae
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-16 16:14:49 +0100

    adjusted the links in the footer so the animation only triggers once

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,17 @@ function Footer() {
 
 
 
-          <motion.div initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .1 }}> <span className="mb-3 mb-sm-0">© 2024 Richard Llewellyn</span></motion.div>
+          <motion.div initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .1 }} viewport={{ once: true }}> <span className="mb-3 mb-sm-0">© 2024 Richard Llewellyn</span></motion.div>
         </div>
 
         <ul className="nav col-md-6 justify-content-end list-unstyled d-flex">
 
-          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .2 }} className="ms-3"><a className="text-decoration-none" target="_blank" href="https://github.com/RichLlew182"><i
+          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .2 }} viewport={{ once: true }} className="ms-3"><a className="text-decoration-none" target="_blank" href="https://github.com/RichLlew182"><i
             className="fa-brands fa-github me-2"></i>Github</a></motion.li>
-          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .3 }} className="ms-3"><a className="text-decoration-none" target="_blank" href="https://www.linkedin.com/in/richard-lee-llewellyn"><i
+          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .3 }} viewport={{ once: true }} className="ms-3"><a className="text-decoration-none" target="_blank" href="https://www.linkedin.com/in/richard-lee-llewellyn"><i
             className="fa-brands fa-linkedin  me-2"></i>Linkedin</a></motion.li>
 
-          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .4 }} className="ms-3"><a className="text-decoration-none" target="_blank" href="./assets/files/richard-llewellyn-cv.pdf"><i className="fa-solid fa-file-lines me-2"></i>Resume</a></motion.li>
+          <motion.li initial={{ opacity: 0, y: -40 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: .4 }} viewport={{ once: true }} className="ms-3"><a className="text-decoration-none" target="_blank" href="./assets/files/richard-llewellyn-cv.pdf"><i className="fa-solid fa-file-lines me-2"></i>Resume</a></motion.li>
 
 
         </ul>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,13 @@ function Header() {
       <header>
         <Navbar collapseOnSelect expand="lg" className="py-3 bg-white">
           <Container className="px-4 px-sm-0">
-            <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
-            <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
+            <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }} viewport={{ once: true }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
+            <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
               <Nav>
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }} viewport={{ once: true }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
               </Nav>
             </Navbar.Collapse>
           </Container>
