bd4b527d7201c7b73ace4823dc47bad3be05841f
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-22 18:59:25 +0100

    adjusted bootstrap padding values across the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,11 +32,11 @@ function Header() {
     <>
       <header>
         <Navbar collapseOnSelect expand="lg" className="py-3 bg-body">
-          <Container className="mx-4 px-0 mx-sm-auto">
+          <Container className="px-5 px-lg-0 mx-sm-auto">
 
             <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }} viewport={{ once: true }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
 
-            <motion.div id="mobile-dark-toggle" className="d-block d-lg-none" initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon text-primary"></i>}</Nav.Link></motion.div>
+            <motion.div id="mobile-dark-toggle" className="d-block d-lg-none me-5 me-lg-0" initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon text-primary"></i>}</Nav.Link></motion.div>
 
             <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
 
--- src/pages/ProjectGallery.jsx
+++ src/pages/ProjectGallery.jsx
@@ -21,12 +21,12 @@ function ProjectGallery() {
     <>
 
       <section id="projects" className="primary-green">
-        <div className="container py-5 px-4 px-sm-0">
+        <div className="container py-5 px-5 px-lg-0">
           <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.25 }} className="mb-4 display-4 text-highlight">Projects</motion.h1>
           <div className="row">
             {
               projectList.map((project, index) => (
-                <motion.div className='col-md-6 col-lg-4 my-3' key={project.id} variants={fadeInAnimationVariants} viewport={{ once: true }} whileInView={"animate"} initial={"initial"} custom={index}>
+                <motion.div className='col-md-12 col-lg-4 my-3' key={project.id} variants={fadeInAnimationVariants} viewport={{ once: true }} whileInView={"animate"} initial={"initial"} custom={index}>
                   <ProjectCard key={project.id} title={project.title} description={project.description} image={project.image} altText={project.imageAltText} techOne={project.tech[0]} techTwo={project.tech[1]} techThree={project.tech[2]} deployedURL={project.deployedURL} repoLink={project.repoLink} />
                 </motion.div>
               ))
