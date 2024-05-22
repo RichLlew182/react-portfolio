781ad8ef1d2d1b330020dbbe7741e981ba982c1b
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-22 17:13:04 +0100

    added extra dark mode toggle that only displays in top menu on smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,16 +25,21 @@ function Header() {
     <>
       <header>
         <Navbar collapseOnSelect expand="lg" className="py-3 bg-body">
-          <Container className="px-4 px-sm-0">
+          <Container className="mx-4 px-0 mx-sm-auto">
+
             <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }} viewport={{ once: true }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
+
+            <motion.div id="mobile-dark-toggle" className="d-block d-lg-none" initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon text-primary"></i>}</Nav.Link></motion.div>
+
             <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
 
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
               <Nav className="gap-2">
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }} viewport={{ once: true }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon text-primary"></i>}</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2 text-center" to="/">Home</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2 text-center" to="/projects">Projects</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }} viewport={{ once: true }}><Nav.Link as={Link}
+                  className="text-center" to="/contact">Contact</Nav.Link></motion.div>
+                <motion.div className="d-none d-lg-block" initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon text-primary"></i>}</Nav.Link></motion.div>
               </Nav>
             </Navbar.Collapse>
 
