c4a99e247a23826eabc465d58b9b6794bd8b75b5
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-05-22 16:39:48 +0100

    added css for dark mode elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,15 +28,18 @@ function Header() {
           <Container className="px-4 px-sm-0">
             <motion.div initial={{ opacity: 0, y: 0 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: .8 }} viewport={{ once: true }}><Navbar.Brand as={Link} to="/">richllew.dev</Navbar.Brand></motion.div>
             <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Navbar.Toggle aria-controls="responsive-navbar-nav" /></motion.div>
+
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-              <Nav>
+              <Nav className="gap-2">
                 <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.1 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/">Home</Nav.Link></motion.div>
                 <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.2 }} viewport={{ once: true }}><Nav.Link as={Link} className="me-lg-2" to="/projects">Projects</Nav.Link></motion.div>
                 <motion.div initial={{ opacity: 0, y: -40 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.3 }} viewport={{ once: true }}><Nav.Link as={Link} to="/contact">Contact</Nav.Link></motion.div>
-                <motion.div initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun"></i> : <i className="fa-solid fa-moon"></i>}</Nav.Link></motion.div>
+                <motion.div initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun text-highlight"></i> : <i className="fa-solid fa-moon"></i>}</Nav.Link></motion.div>
               </Nav>
             </Navbar.Collapse>
+
           </Container>
+          <motion.div initial={{ opacity: 0, y: -40 }} onClick={handleDarkMode} animate={{ opacity: 1, y: 0 }} transition={{ delay: 1.4 }} viewport={{ once: true }}><Nav.Link>{darkMode ? <i className="fa-solid fa-sun"></i> : <i className="fa-solid fa-moon"></i>}</Nav.Link></motion.div>
         </Navbar>
       </header>
     </>
--- src/pages/ProjectGallery.jsx
+++ src/pages/ProjectGallery.jsx
@@ -22,7 +22,7 @@ function ProjectGallery() {
 
       <section id="projects" className="primary-green">
         <div className="container py-5 px-4 px-sm-0">
-          <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.25 }} className="mb-4 display-5">Projects</motion.h2>
+          <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.25 }} className="mb-4 display-5 text-highlight">Projects</motion.h2>
           <div className="row">
             {
               projectList.map((project, index) => (
